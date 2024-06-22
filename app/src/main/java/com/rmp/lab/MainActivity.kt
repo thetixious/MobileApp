ececfd929e2f1b7fd0413af5410b5b3f2df4988c
Arkadiy <ri.arkadiy@gmail.com>
2024-06-22 23:58:15 +0300

    minor changes

--- app/src/main/java/com/rmp/lab/MainActivity.kt
+++ app/src/main/java/com/rmp/lab/MainActivity.kt
@@ -1,20 +1,14 @@
 package com.rmp.lab
 
-import android.icu.util.Calendar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.rmp.lab.ui.theme.SmarthouseTheme
 
@@ -29,6 +23,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+
                 }
             }
         }
